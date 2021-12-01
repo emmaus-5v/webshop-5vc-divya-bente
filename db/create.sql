9e1b6285d48ef093bbae96b4276ba96d26990e2d
Bentedb <112315@emmauscollege.nl>
2021-12-01 09:08:47 +0000

    beschrijving korter gemaakt/veranderd
    Signed-off-by: Bentedb <112315@emmauscollege.nl>

--- db/create.sql
+++ db/create.sql
@@ -20,6 +20,7 @@ CREATE TABLE kleur (
 );
 
 
+
 --
 -- populate with data
 --
@@ -29,26 +30,18 @@ CREATE TABLE kleur (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Oui x Oui Baquette Tas', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
+insert into products (name, description, code, price) values 
+('Oui x Oui Baquette Tas', 'leuke tas met koeien print', '816905633-0', 15.00);
 
-Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
+insert into products (name, description, code, price) values 
+('Dress Bente', 'mooie rode jurk', '077030122-3', 19.95);
 
-In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', '816905633-0', 15.00);
-insert into products (name, description, code, price) values ('Dress Bente', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.
+insert into products (name, description, code, price) values
+ ('TankTop With Long Sleeves', 'beautiful tanktop sagegreen ', '445924201-X', 13.5);
 
-Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
+insert into products (name, description, code, price) values 
+('Dress Divya', 'just as beautiful as divya', '693155505-7', 13.5);
 
-Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', '077030122-3', 19.95);
-insert into products (name, description, code, price) values ('TankTop With Long Sleeves', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.
-
-Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-
-Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Dress Divya', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
-
-In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.
-
-Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.', '693155505-7', 13.5);
 insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
 insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
 
