1f38258fb5cd4a470bf679a9165f97540a837cfe
Bentedb <112315@emmauscollege.nl>
2021-12-20 11:16:52 +0000

    database toevoeging
    Signed-off-by: Bentedb <112315@emmauscollege.nl>

--- db/create.sql
+++ db/create.sql
@@ -19,6 +19,13 @@ CREATE TABLE kleur (
   
 );
 
+DROP TABLE IF EXISTS product_kleur; 
+CREATE TABLE product_kleur (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+ product_id INTEGER,
+ kleur_id INTEGER
+);
+
 DROP TABLE IF EXISTS categorie;
 CREATE TABLE categorie (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -50,7 +57,7 @@ insert into products (name, description, code, price, kleur_id) values
 ('Dress Bente', 'mooie rode jurk', '077030122-3', 19.95,2);
 
 insert into products (name, description, code, price, kleur_id) values
- ('TankTop With Long Sleeves', 'beautiful tanktop sagegreen ', '445924201-X', 13.5,3);
+ ('TankTop With Long Sleeves', 'beautiful tanktop sagegreen ', '445924201-X', 13.5, 3);
 
 insert into products (name, description, code, price, kleur_id) values 
 ('Dress Divya', 'just as beautiful as divya', '693155505-7', 13.5,4);
@@ -59,21 +66,30 @@ insert into products (name, description, code, price, kleur_id) values
 ('Butterfly broek en trui', 'zwart joggingspak met vlinders', '686928463-6', 14, 3);
 
 insert into products (name, description, code, price, kleur_id) values 
-('Hoodie New York multi color', 'comfortabele hoodie boyfriend material', '492662523-7', 14,6);
+('Hoodie New York multi color', 'comfortabele hoodie boyfriend material', '492662523-7', 14, 6);
 
 insert into products (name, description, code, price, kleur_id) values 
-('joggingspak baby blue', 'zachte stof joggingspak met tanktop', '492662523-7', 14,5);
+('joggingspak baby blue', 'zachte stof joggingspak met tanktop', '492662523-7', 14, 5);
 
 insert into products (name, description, code, price, kleur_id) values 
 ('bliksem joggingspak ', 'zwart joggingspak bliksem', '492662523-7', 14, 3);
 
 
-INSERT INTO kleur (id,name) values (1,'zwart-wit');
-INSERT INTO kleur (id,name) values (2,'rood');
-INSERT INTO kleur (id,name) values (3,'zwart');
-INSERT INTO kleur (id,name) values (4,'paars');
-INSERT INTO kleur (id,name) values (5,'baby-blauw');
-INSERT INTO kleur (id,name) values (6,'bruin-beige');
+INSERT INTO kleur (name, id) values ('zwart-wit', 1);
+INSERT INTO kleur (name, id) values ('rood', 2);
+INSERT INTO kleur (name, id) values ('zwart', 3);
+INSERT INTO kleur (name, id) values ('paars', 4);
+INSERT INTO kleur (name, id) values ('baby-blauw', 5);
+INSERT INTO kleur (name, id) values ('bruin-beige', 6);
+
+INSERT INTO kledingstuk_kleur (kledingstuk_id, kleur_id) values (1, 1);
+INSERT INTO kledingstuk_kleur (kledingstuk_id, kleur_id) values (2, 2);
+INSERT INTO kledingstuk_kleur (kledingstuk_id, kleur_id) values (3, 3);
+INSERT INTO kledingstuk_kleur (kledingstuk_id, kleur_id) values (4, 4);
+INSERT INTO kledingstuk_kleur (kledingstuk_id, kleur_id) values (5, 3);
+INSERT INTO kledingstuk_kleur (kledingstuk_id, kleur_id) values (6, 6);
+INSERT INTO kledingstuk_kleur (kledingstuk_id, kleur_id) values (7, 5);
+INSERT INTO kledingstuk_kleur (kledingstuk_id, kleur_id) values (8, 3);
 
 INSERT INTO categorie (name) values ('tas');
 INSERT INTO categorie (name) values ('jurk');
